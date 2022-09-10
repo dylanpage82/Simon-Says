6570dfaf53f7d077c414c970d018da99688b547b
Dylan <dylanpage82@gmail.com>
2022-09-10 16:37:37 -0500

    Got the basic design done

---
+++ script.js
@@ -0,0 +1,4 @@
+// playerChoices = []
+// computerChoices = []
+// Function that fires off when you click start or when the player hits a right answer that simply randomly adds one of the four colors to the computerChoices
+// Then when the player clicks one of them, you just check if that matches the computer's choice
