2cde91c0e4c43b5acc04c9f9855a1edcb25e0cfb
Dylan <dylanpage82@gmail.com>
2022-09-19 21:40:10 -0500

    Latest model of game

--- Readme.md
+++ Readme.md
@@ -5,7 +5,7 @@
 
 ## Game Description
 
-    Simon says is a game where you have to follow along with what the computer chose and choose the right answer.
+    The Simon game is the exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order. It's fast-paced play, with lights and sounds that can challenge you.
 
 ## How To Play
 
--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let correctChoice = true;///determines if the players choice = computers choice
 const level = document.querySelector('.level');
 const modal = document.getElementById('modal');
 const beginnerButton = document.querySelector('.beginner');
+let computerTurn=true;
 const intermediateButton = document.querySelector('.intermediate');
 const hardButton = document.querySelector('.hard');
 let beginner = false;
@@ -17,65 +18,73 @@ let intermediate = false;
 let hard = false;
 let rank = false;///level selected (can be any level just a indicator)
 let win = false;
-let computerPlays;///number of times the computer turn has ran
+let computerPlays = 0;//computer choices array index indicator since i resets
+let playerTurn = 1;
+let intervalEl = 0;//have to set interval to something so it can be cleared
 
 
-function play () {
-    computerPlays = 0;
-    intervalId = 0;
-    playerTurn = 1;
-    counter.innerHTML= playerTurn;
-    for(i=0; i<=20; i++){
-         computerChoices.push(Math.ceil(Math.random() *4));
-    }
-    computerTurn = true;
-    intervalId=setInterval(gameRound, 1000)
+
+
+
+//iterates a array of 20 numbers and sets game speed
+const play = () => {
+    
+     for(i=0; i<=20; i++){
+          computerChoices.push(Math.ceil(Math.random() *4));
+     }
+    
+        intervalEl=setInterval(gameRound, 800)
 }
-function gameRound(){
-
+//Determines whose turn it is, if computers turn sets the index of the array to display a certian style
+const gameRound = () => {
+    
     if(computerPlays == playerTurn){
-        clearInterval(intervalId);
+        clearInterval(intervalEl);
         computerTurn = false;
         resetLight();
     }
+
     if(computerTurn){
         resetLight();
-        setTimeout(() => {
+        setTimeout(() =>{
+            
             if(computerChoices[computerPlays] ==1){
                 green.classList.add('active')
                 const audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
                 audio.play();  
-            }
-            if(computerChoices[computerPlays] ==2){
+            }else if(computerChoices[computerPlays] ==2){
                 red.classList.add('active')
                 const audio = new Audio(`https://s3.amazonaws.com/freecodecamp/simonSound2.mp3`);
                 audio.play();   
-            }
-            if(computerChoices[computerPlays] ==3){
+            }else if(computerChoices[computerPlays] ==3){
                 yellow.classList.add('active')
                 const audio = new Audio(`https://s3.amazonaws.com/freecodecamp/simonSound3.mp3`);
                 audio.play();
-            }
-            if(computerChoices[computerPlays] ==4){
+            }else if(computerChoices[computerPlays] ==4){
                 blue.classList.add('active')
                 const audio = new Audio(`https://s3.amazonaws.com/freecodecamp/simonSound4.mp3`);
                 audio.play();
             }
             computerPlays++
-        }, 300);
-        }
+       
+        }, 500)
     }
-function resetLight(){
+}
+//Just clears out the lights
+const resetLight = ()=>{
     green.classList.remove('active');
     red.classList.remove('active');
     yellow.classList.remove('active');
     blue.classList.remove('active');
     
 }
-function checkAnswers(){
+
+const checkAnswers =() =>{
+    //checks to make sure player is still making the write choice
     if(playerChoices[playerChoices.length -1] !== computerChoices[playerChoices.length -1]){
         correctChoice = false;
     }
+    //checks to see if player won
     if(beginner === true &&  playerChoices.length ==5 && correctChoice){
         winGame()
     }else if(intermediate === true &&  playerChoices.length ==10 && correctChoice){
@@ -90,19 +99,21 @@ function checkAnswers(){
         }, 3000);
         
     }
+    //if player is still making correct choices then moves on to the computers turn
     if(playerTurn == playerChoices.length && correctChoice && !win){
         playerTurn++
         playerChoices = [];
         computerTurn = true;
         computerPlays = 0;
         counter.innerHTML = playerTurn;
-        intervalId = setInterval(gameRound, 1000);
+        intervalEl = setInterval(gameRound, 800);
     }
 }
-function openModal(){
+
+const openModal = ()=>{
     modal.classList.add('open')
 }
-function closeModal(){
+const closeModal =() =>{
     modal.classList.remove('open')
 }
 
@@ -132,6 +143,7 @@ hardButton.addEventListener('click', () =>{
 })
 
 green.addEventListener('click', () =>{
+    //makes it to were you cant click when its the computers turn
     if(computerTurn === true){
         return;
     }else{
@@ -147,7 +159,7 @@ red.addEventListener('click', () =>{
         return;
     }else{
     playerChoices.push(2);
-    const audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+    let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
     audio.play(); 
     checkAnswers();
 
@@ -159,7 +171,7 @@ yellow.addEventListener('click', () =>{
         return;
     }else{
     playerChoices.push(3);
-    const audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+    let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
         audio.play(); 
     checkAnswers();
     }
@@ -170,22 +182,24 @@ blue.addEventListener('click', () => {
         return;
     }else{
     playerChoices.push(4);
-    const audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+    let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
     audio.play(); 
     checkAnswers();
     }
 });
 
 start.addEventListener('click', () =>{
+    //forces you to select a level before you can play the game
     if(rank != true){
         alert('Please Select a level');
     }else if(rank === true){
     play();
+    counter.innerHTML= playerTurn;
     }
 });
 
 
-function winGame(){
+const winGame = () => {
     counter.innerHTML = 'YOU WIN!'
     win = true;
 }
